42f58ecf1b8e387360b7253cc5eb279c91dd5ab4
DalerKulchukov <laxusdreyar20020709@gmail.com>
2023-06-17 14:47:43 +0600

    dz2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
 
-function App() {
+// class Square extends React.Component {
+
+//   constructor(props){
+//     super(props);
+//     this.state={
+//       value:null,
+//     };
+//   }
+
+
+//   render() {
+//     return (
+//       <button className="square" 
+//       onClick={()=> this.setState({value:"0"})}>
+//         {this.state.value}
+//       </button>
+//     )
+//   }
+// }
+
+// class Board extends React.Component {
+//   renderSquare(i) {
+//     return <Square />;
+//   }
+
+//   render() {
+//     const status = "Next player: 0";
+
+//     return (
+//       <div>
+//         <div className="status">{status}</div>
+//         <div className="board-row">
+//           {this.renderSquare(0)}
+//           {this.renderSquare(1)}
+//           {this.renderSquare(2)}
+//         </div>
+
+//         <div className="board-row">
+//           {this.renderSquare(3)}
+//           {this.renderSquare(4)}
+//           {this.renderSquare(5)}
+//         </div>
+
+//         <div className="board-row">
+//           {this.renderSquare(6)}
+//           {this.renderSquare(7)}
+//           {this.renderSquare(8)}
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+
+// class Game extends React.Component {
+//   render() {
+//     return (
+//       <div className='game'>
+//         <div className="game-board">
+//           <Board />
+//         </div>
+//         <div className='game-info'>
+//           <div></div>
+//           <ol></ol>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+
+// export default Game;
+
+import React  from "react";
+import { useState } from 'react';
+
+// const App = () => {
+
+//   const [state,setState] = useState(2);
+//   const [index,setIndex] = useState(1);
+  
+
+
+
+//   return (
+//     <div>
+//       <div class="red"></div>
+//       <div class="blue"></div>
+//     </div>
+//   );
+// };
+const App = ()=> {
+  const [ cubes, setCubes ] = React.useState(2);
+  const [ index, setIndex ] = React.useState(1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="cubes">
+      {[...Array(cubes).keys()].map((_, i) => 
+      	<p
+          key={i}
+          onClick={() => setIndex(i)}
+          style={{ backgroundColor: index === i ? 'blue' : 'red' }}
+        ></p>
+        )}
     </div>
   );
 }
-
 export default App;
